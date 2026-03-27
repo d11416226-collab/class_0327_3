4e6452a241b87ccaf3f3eb2ca34ebc93d021bce7
d11416226-collab <d11416226@gs.takming.edu.tw>
2026-03-27 10:53:46 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace class_0327_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                Test std1 = new Test(78, 72);
+                Test std2 = new Test(68, 84);
+
+            std1.SetMid(88);
+            std2.SetFinal(82);
+
+                Console.WriteLine("1:"+std1.GetMid().ToString());
+                Console.WriteLine("2:"+std2.GetFinal().ToString());
+          }
+    }
+}
